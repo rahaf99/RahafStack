0ad0f6a57be946678bc0d1b22ec312d31bb7e679
rahaf99 <rahaf-ma7moud@hotmail.com>
2021-10-04 16:10:55 +0300

    pushing project to the main branch

---
+++ StackTask/StackUsingList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackTask
+{
+    class StackUsingList<T>
+    {
+        private readonly List<T> _stackList;
+        public StackUsingList()
+        {
+            _stackList = new List<T>();
+        }
+
+        public void push(T a)
+        {
+            _stackList.Add(a);
+        }
+
+        public T pop()
+        {
+            T x = _stackList[_stackList.Count - 1];
+            _stackList.RemoveAt(_stackList.Count - 1);
+            return x;
+        }
+
+        public void print()
+        {
+            if (_stackList.Count > 0)
+            {
+                foreach (var item in _stackList)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        public void peak()
+        {
+            if (_stackList.Count >= 1)
+                Console.WriteLine(_stackList[_stackList.Count - 1].ToString());
+        }
+
+        public void clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
