f4c0f5d9d19e9c91875e2816565940508200101d
rahaf99 <rahaf-ma7moud@hotmail.com>
2021-10-04 21:58:26 +0300

    delete unnecessary print commands in the program file

--- StackTask/Program.cs
+++ StackTask/Program.cs
@@ -9,32 +9,16 @@ namespace StackTask
 				Console.WriteLine("let's start!!");
 				StackUsingList<int> s = new StackUsingList<int>();
 				s.push(1);
-				s.print();
-				Console.WriteLine("---------------");
 				s.push(2);
-				s.print();
-				Console.WriteLine("---------------");
 				s.push(3);
-				s.print();
-				Console.WriteLine("---------------");
 				s.push(4);
-				s.print();
-				Console.WriteLine("---------------");
 				s.push(5);
-				s.print();
-				Console.WriteLine("---------------");
 				s.push(6);
 				s.print();
-				Console.WriteLine("---------------");
 				Console.WriteLine(s.pop());
-				s.print();
-				Console.WriteLine("---------------");
 				s.peak();
-				Console.WriteLine("---------------");
 				s.print();
-				Console.WriteLine("---------------");
 				s.clear();
-				Console.WriteLine("---------------");
 				s.print();
 			}
     }
