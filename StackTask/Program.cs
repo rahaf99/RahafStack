699fef58dbf12d8c06564581972b80012e0f1e92
rahaf99 <rahaf-ma7moud@hotmail.com>
2021-10-04 21:48:43 +0300

    Create StackUsingLinkList file

--- StackTask/Program.cs
+++ StackTask/Program.cs
@@ -6,37 +6,23 @@ namespace StackTask
     {
         static void Main(string[] args)
         {
-				Console.WriteLine("let's start!!");
-				StackUsingList<int> s = new StackUsingList<int>();
-				s.push(1);
-				s.print();
-				Console.WriteLine("---------------");
-				s.push(2);
-				s.print();
-				Console.WriteLine("---------------");
-				s.push(3);
-				s.print();
-				Console.WriteLine("---------------");
-				s.push(4);
-				s.print();
-				Console.WriteLine("---------------");
-				s.push(5);
-				s.print();
-				Console.WriteLine("---------------");
-				s.push(6);
-				s.print();
-				Console.WriteLine("---------------");
-				Console.WriteLine(s.pop());
-				s.print();
-				Console.WriteLine("---------------");
-				s.peak();
-				Console.WriteLine("---------------");
-				s.print();
-				Console.WriteLine("---------------");
-				s.clear();
-				Console.WriteLine("---------------");
-				s.print();
-				Console.WriteLine("---------------");
-			}
+			Console.WriteLine("let's start!!");
+			StackUsingLinkList<int> s = new StackUsingLinkList<int>();
+			s.push(1);
+			s.push(2);
+			s.push(3);
+			s.push(4);
+			s.push(5);
+			s.push(6);
+			s.print();
+			Console.WriteLine("s.pop: "+s.pop());
+			s.print();
+			Console.WriteLine("s.peak: ");
+			s.peak();
+			s.print();
+			Console.WriteLine("s.clear: ");
+			s.clear();
+			s.print();
+		}
     }
 }
---
+++ StackTask/StackUsingLinkList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackTask
+{
+    class StackUsingLinkList<T>
+    {
+        private readonly LinkedList<T> _stackLinkList;
+        public StackUsingLinkList()
+        {
+            _stackLinkList = new LinkedList<T>();
+        }
+
+        public void push(T a)
+        {
+            _stackLinkList.AddLast(a);
+        }
+
+        public T pop()
+        {
+            T x = _stackLinkList.Last.Value;
+            _stackLinkList.RemoveLast();
+            return x;
+        }
+
+        public void print()
+        {
+            if (_stackLinkList.Count > 0)
+            {
+                foreach (var item in _stackLinkList)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        public void peak()
+        {
+            if (_stackLinkList.Count >= 1)
+                Console.WriteLine(_stackLinkList.Last.Value.ToString());
+        }
+
+        public void clear()
+        {
+            _stackLinkList.Clear();
+        }
+    }
+}
